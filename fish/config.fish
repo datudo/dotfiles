14c722a4db9b1e07194a0e1b852e6bd18995912a
datudo <23307130037@m.fudan.edu.cn>
2025-01-17 00:19:04 +0800

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# use y instead of yazi to start, and press q to quit, you'll see the CWD changed. Sometimes, you don't want to change, press Q to quit
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+bind \t\\ forward-char
---
+++ nvim/init.lua
@@ -0,0 +1,2 @@
+vim.opt.fileencoding = "utf-8"  -- 设置文件编码为 utf-8
+vim.opt.encoding = "utf-8"      -- 设置 Neovim 内部编码为 utf-8
